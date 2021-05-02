d7ccb60f344cab5ee873cef7be3bad0d84bde001
rubenconnect <francosanchezruben@gmail.com>
2021-05-02 17:03:20 +0100

    adding select and filtersSort component

---
+++ src/components/select.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SelectContainer = styled.div`
+  ${({ borderRadious }) => `${borderRadious && `border-radius: 21px 0 0 21px`}`};
+  ${({ borderRadious }) => `${!borderRadious && `margin-right: 5px`}`};
+  display: flex;
+  align-items: center;
+  height: 40px;
+  background-color: #215184;
+  color: #ffffff;
+  margin: 15px;
+  font-size: 16px;
+  font-family: 'Brandon Grotesque', Arial, Helvetica, sans-serif;
+  font-weight: 700;
+`;
+
+const SelectComponent = styled.select`
+  ${({ borderRadious }) => `${borderRadious && `border-radius: 21px 0 0 21px`}`};
+  ${({ borderRadious }) => `${!borderRadious && `margin-right: 5px`}`};
+  background-color: #215184;
+  color: #ffffff;
+  font-size: 16px;
+  font-family: 'Brandon Grotesque', Arial, Helvetica, sans-serif;
+  font-weight: 700;
+`;
+
+const SelectText = styled.p`
+  margin: 15px;
+`;
+
+const StyledOption = styled.option`
+  color: #ffffff;
+  background-color: #215184;
+  margin: 15px;
+  font-size: 16px;
+  font-family: 'Brandon Grotesque', Arial, Helvetica, sans-serif;
+  font-weight: 700;
+`;
+
+type Props = {
+  text: String,
+  icon: File,
+  borderRadious: Boolean,
+  options: Array,
+};
+
+export default function Button({
+  text,
+  borderRadious,
+  options,
+}: Props) {
+
+  const optionsComponent = options.map(option => (
+    <StyledOption key={option} value={option}>
+      {option}
+    </StyledOption>
+  ));
+
+  return (
+    <SelectContainer>
+      <SelectText>
+        {text}
+      </SelectText>
+      <SelectComponent 
+        borderRadious={borderRadious}
+      >
+        <StyledOption value="" />
+        {optionsComponent}
+      </SelectComponent>
+    </SelectContainer>
+  );
+};
--- src/containers/filtersSort.js
+++ src/containers/filtersSort.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '../components/button';
-import sortIcon from '../assets/icon/sort.png';
-import filterByYearIcon from '../assets/icon/select.png';
+import Select from '../components/select';
 
 const Container = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
 
-const sortText = 'Reload Data';
+const sortText = 'Sort Descending';
 const filterByYearText = 'Filter by Year';
 
-export default function FiltersSort() {
+type Props = {
+  allLaunches: Array;
+};
+
+export default function FiltersSort({
+  allLaunches,
+}: Props) {
+  const filteredYears = () => {
+    const launchYears = allLaunches && allLaunches.map(launch => launch.launch_year);
+
+    return launchYears && launchYears.reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], []);
+  };
+
   return (
     <Container>
-      <Button text={filterByYearText} icon={filterByYearIcon}/>
-      <Button text={sortText} icon={sortIcon}/>
+      {filteredYears() && 
+        <Select 
+          text={filterByYearText} 
+          options={filteredYears()}
+        />
+      }
+      {/* <Select text={sortText} icon={sortIcon}/> */}
     </Container>
   );
 }
